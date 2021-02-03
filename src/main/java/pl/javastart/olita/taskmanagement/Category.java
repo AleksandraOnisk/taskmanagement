b502d1c7cbb9874f8fbe266e8fdad32fe6988553
Aleksandra Onisk <aleksandra.siciarz@gmail.com>
2021-02-03 10:18:50 +0100

    commit

--- src/main/java/pl/javastart/olita/taskmanagement/Category.java
+++ src/main/java/pl/javastart/olita/taskmanagement/Category.java
@@ -2,6 +2,15 @@ package pl.javastart.olita.taskmanagement;
 
 public enum Category {
 
-    HOME, WORK, PRIVATE
+    HOME("dom"), WORK("praca"), PRIVATE("prywatne");
 
+    private String displayName;
+
+    Category(String displayName){
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
--- src/main/java/pl/javastart/olita/taskmanagement/TaskController.java
+++ src/main/java/pl/javastart/olita/taskmanagement/TaskController.java
@@ -1,7 +1,45 @@
 package pl.javastart.olita.taskmanagement;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TaskController {
+
+    private TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+
+        List<Task> tasksList = taskRepository.findAll();
+
+        model.addAttribute("tasksList", tasksList);
+
+        return "home";
+    }
+
+    @GetMapping("/task/{id}")
+    public String showTask(@PathVariable Long id, Model model){
+        Optional<Task> taskOptional = taskRepository.findById(id);
+
+        if(taskOptional.isPresent()){
+            Task task = taskOptional.get();
+            model.addAttribute("task", task);
+
+            return "task";
+        } else {
+            return "redirect:/";
+        }
+
+    }
+
 }
